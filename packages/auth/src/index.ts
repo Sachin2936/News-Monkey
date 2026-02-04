6389c63c91142958448212a6d545b682b6559dc6
Sachin Singh <sachin31033@gmail.com>
2026-02-04 14:53:43 +0530

    auth cors

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,17 +4,13 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 const isProduction = env.NODE_ENV === "production";
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "https://news-monkey-six-rho.vercel.app",
-];
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
 
-  trustedOrigins: allowedOrigins,
+  trustedOrigins: env.CORS_ORIGIN,
   emailAndPassword: {
     enabled: true,
   },
